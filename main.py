ea81cd8a53d83d7db2dc835555b2be35e0f6fd05
fran <phon.youngmeesuk@gmail.com>
2023-10-25 23:21:24 +0700

    [add] path move set
    the move is still need to implement a lil more

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import numpy as np
 import cv2
 import math
-
+import time
 # Import robot controller class
 from controller.robot import Robot
 
@@ -21,8 +21,111 @@ Port = '1234'
 res = fetch_dir(IP, Port, cv2.imread(r'./BuildModel/lil_left.png'))
 print(res)
 
+last_ag = 0
 # Loop overtime
 while True:
+    
+
+
+
+    leaf_collector.set_mxSpeed(255)
+
+    #cp 0
+    leaf_collector.forward(last_ag, 0)
+    #if last_ag is not 0 then start timer
+    if last_ag != 0:
+        start_time = time.time()
+
+    # move straight without turning
+    #cp 1
+    #if time passed 10 second then turn left
+    if time.time() - start_time > 10:
+        while last_ag != -90:
+            leaf_collector.forward(last_ag, -90)
+             #wait for 1 second before next iteration
+            time.sleep(1)
+            #reset timer 
+            start_time = time.time()
+            break
+
+    leaf_collector.forward(last_ag, 0)
+    #cp2
+    #if time passed 10 second then turn left
+    if time.time() - start_time > 10:
+        while last_ag != -90:
+            leaf_collector.forward(last_ag, -90)
+            time.sleep(1)
+            #reset timer
+            start_time = time.time()
+            break
+
+
+
+    leaf_collector.forward(last_ag, 0)
+    #cp 2
+    #if time passed 10 second then turn left
+    if time.time() - start_time > 10:
+        while last_ag != -90:
+            leaf_collector.forward(last_ag, -90)
+            time.sleep(1)
+            #reset timer
+            start_time = time.time()
+            break
+
+    leaf_collector.forward(last_ag, 0)
+
+    #cp 3
+    if time.time() - start_time > 5:
+        while last_ag != -90:
+            leaf_collector.forward(last_ag, -90)
+            time.sleep(1)
+            #reset timer
+            start_time = time.time()
+            break
+    leaf_collector.forward(last_ag, 0)
+    #cp 4
+    if time.time() - start_time > 5:
+        while last_ag != -90:
+            leaf_collector.forward(last_ag, -90)
+            time.sleep(1)
+            #reset timer
+            start_time = time.time()
+            break
+    #cp 5 ++
+    for i in range(0, 10):
+        leaf_collector.forward(last_ag, 0)
+
+        if time.time() - start_time > 10:
+            while last_ag != 90:
+                leaf_collector.forward(last_ag, 90)
+                time.sleep(1)
+                #reset timer
+                start_time = time.time()
+                break
+        leaf_collector.forward(last_ag, 0)
+        if time.time() - start_time > 3:
+            while last_ag != 90:
+                leaf_collector.forward(last_ag, 90)
+                time.sleep(1)
+                #reset timer
+                start_time = time.time()
+                break
+        leaf_collector.forward(last_ag, 0)
+        if time.time() - start_time > 10:
+            while last_ag != -90:
+                leaf_collector.forward(last_ag, -90)
+                time.sleep(1)
+                #reset timer
+                start_time = time.time()
+                break
+        leaf_collector.forward(last_ag, 0)
+        if time.time() - start_time > 3:
+            while last_ag != -90:
+                leaf_collector.forward(last_ag, -90)
+                time.sleep(1)
+                #reset timer
+                start_time = time.time()
+                break
 
 
     break
